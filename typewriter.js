808cb3a31d1adee5e252d4ffbc093121ec84deb1
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2019-12-03 19:35:41 +0000

    refactor the function a little bit

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,27 @@
+// const sentence = "hello there from lighthouse labs";
+// let i = 0;
+// const typewriter = function(str) {
+//   if(i < str.length) {
+//     for(const char of str) {
+//       setTimeout(function() {
+//         process.stdout.write(char);
+//       }, 300*i)
+//       i++;
+//     }
+//   }
+//   console.log("\n");
+// }
+// typewriter(sentence);
+
 const sentence = "hello there from lighthouse labs";
-let i = 0;
-const typewriter = function(str) {
-  if(i < str.length) {
-    for(const char of str) {
-      setTimeout(function() {
-        process.stdout.write(char);
-      }, 300*i)
-      i++;
-    }
+const typewriter = function(takeinStr) {
+  let i = 0;
+  for (const char of takeinStr) {
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, i * 300);
+    i ++;
   }
 }
+
 typewriter(sentence);
